7ef20698fb7c00a71005225f38e6ee0cc286f388
Kunalshirke <86478462+Kunalshirke@users.noreply.github.com>
2021-06-27 10:08:24 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Header() {
+  return(<header>
+    <h1>
+    bootcamp by kunal technology
+    </h1>
+    </header >);
+
+}
+export default Header;
---
+++ Info.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+function Info() {
+  return(<div className="note">
+    <h1>Ml bootcamp by kunal technology</h1>
+      <p>basic mL bootcamp</p>
+      </ div>
+  );
+}
+export default Info;
